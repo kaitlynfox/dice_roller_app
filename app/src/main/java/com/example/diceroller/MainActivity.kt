c1e67589bc886dd5883c019e23771aaabd1777ba
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-09-20 12:57:41 -0600

    Create rollDice function, add Dice class, and update onCreate() to display new roll of dice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import org.w3c.dom.Text
 
+// This class allows a user to roll a dice and display the result to the screen
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,8 +16,24 @@ class MainActivity : AppCompatActivity() {
         // Stores a reference to this specific button through the variable rollButton (Not Button itself)
         // (R.id.button) is the resource id for this specific button (unique identifier)
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
-        }
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    // Rolls the dice and updates the screen with the result
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        val resultTextView: TextView = findViewById(R.id.diceTextView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+// This class creates a dice that has a specific number of sides
+class Dice(private val numberOfSides: Int) {
+
+    // Returns a random number based on the number of sides the dice has
+    fun roll(): Int {
+        return (1..numberOfSides).random()
     }
 }
